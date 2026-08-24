e9a3effeea04364a35abc54173092c73e9ec5c14
vedanshgupta-art <vedanshgupta27boy@gmail.com>
2026-08-24 22:10:16 +0530

    Add files via upload

---
+++ trial.py
@@ -0,0 +1,49 @@
+#Part-1
+
+# name = "Tony stark"
+# age = 53
+# height = 1.85
+# itis = input("Who is he: ")
+# print(name, "is secretly a", itis)
+# print("age=" ,age)
+# print("height=" ,height)
+
+
+#Part-2
+
+# num1= float(input("Enter first value: "))
+# num2= float(input("Enter second value: "))
+# num3= float(input("Enter third value: "))
+# sum = num1+num2+num3
+# avg = sum/3
+# print("The sum of three numbers",sum)
+# print("The avrage value of three numbers ",avg) 
+
+
+#Part-3\Calculator
+
+# a= int(input("Enter the value of a: "))
+# b= int(input("Enter the value of b: "))
+# opt =input("Enter the operator(+,-,*,/ ): ")
+# if opt == '+':
+#     sum= a+b
+#     print(sum)
+# if opt == '-':
+#     sum= a-b
+#     print(sum)
+# if opt == '*':
+#     sum= a*b
+#     print(sum)
+# if opt == '/':
+#     sum= a/b
+#     print(sum)
+# else:
+#     print("inavalid operator")
+
+#Task
+
+
+# for i in range(3,20,2):
+#         print(i)
+for i in range(57,627,57):
+    print(i)
